2daea492193862f09893bc42a95635caea1fd9f9
randalln559 <115368266+randalln559@users.noreply.github.com>
2022-11-21 16:31:00 -0700

    Added newIntern, interns routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,12 +5,16 @@ const {
   mainPage,
   changePassword,
   logout,
+  newIntern,
+  interns,
 } = require("../controllers/userControllers");
 
 const { ensureAuthenticated } = require("../../ensure");
 router.route("/signup").post(signUp);
 router.route("/login").post(login);
-router.route("/main").get(ensureAuthenticated, mainPage);
-router.route("/change").post(changePassword, login);
+router.route("/newIntern").post(newIntern);
+router.route("/interns").get(interns);
+// router.route("/main").get(ensureAuthenticated, mainPage);
+// router.route("/change").post(changePassword, login);
 router.route("/logout").get(logout);
 module.exports = router;

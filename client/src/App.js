a3ea15f6b75a5385c3df5e52598b4d11b7ca2fe3
randalln559 <115368266+randalln559@users.noreply.github.com>
2022-11-21 16:36:45 -0700

    Removed BrowserRouter & added the _id to routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,17 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import { useState } from 'react';
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
-import Register from './components/Register/Register';
+import Register from "./components/Register/Register";
 import Main from "./components/Main/Main";
 
-
 function App() {
-
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <LandingPage />
-            }
-          />
-          <Route
-            exact
-            path="/register"
-            element={<Register />}
-          />
-          <Route
-            exact
-            path="/main:_id"
-            element={<Main />}
-          />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/main:_id" element={<Main />} />
+      </Routes>
     </div>
   );
 }
